cba3f0ba5329bf418fe3405d0b7d7abb423e288a
lalith <lalithnimmagadda@gmail.com>
2020-01-05 22:39:18 -0600

    changes to eigenfaces.py

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -16,19 +16,19 @@ The dataset used in this example is a preprocessed excerpt of the
 
 
 
-print __doc__
+print(__doc__)
 
 from time import time
 import logging
 import pylab as pl
 import numpy as np
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_lfw_people
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import PCA as RandomizedPCA
 from sklearn.svm import SVC
 
 # Display progress logs on stdout
@@ -38,77 +38,96 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 ###############################################################################
 # Download the data, if not already on disk and load it as numpy arrays
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
-
 # introspect the images arrays to find the shapes (for plotting)
 n_samples, h, w = lfw_people.images.shape
+print("type of lfw_people is:",type(lfw_people))
+print("shape of lfw_people.images is:",lfw_people.images.shape)
+print(n_samples)
+print(h)
+print(w)
 np.random.seed(42)
 
 # for machine learning we use the data directly (as relative pixel
 # position info is ignored by this model)
 X = lfw_people.data
+print(X)
+print(X.shape)
 n_features = X.shape[1]
-
+print(n_features)
+print(X[0])
 # the label to predict is the id of the person
 y = lfw_people.target
 target_names = lfw_people.target_names
 n_classes = target_names.shape[0]
-
-print "Total dataset size:"
-print "n_samples: %d" % n_samples
-print "n_features: %d" % n_features
-print "n_classes: %d" % n_classes
+print("number of target names:",n_classes)
+print("Total dataset size:")
+print(("n_samples: %d" % n_samples))
+print("n_features: %d" % n_features)
+print("n_classes: %d" % n_classes)
 
 
 ###############################################################################
 # Split into a training and testing set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-###############################################################################
+# ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+# n_components = 150
+n_components = [10, 15, 25, 50, 100, 250]
 
-print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
-t0 = time()
-pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
-print "done in %0.3fs" % (time() - t0)
+for i in n_components:
+  print("Extracting the top %d eigenfaces from %d faces" % (i, X_train.shape[0]))
+  t0 = time()
+  pca = RandomizedPCA(n_components=i,svd_solver='randomized', whiten=True).fit(X_train)
+  print("done in %0.3fs" % (time() - t0))
 
-eigenfaces = pca.components_.reshape((n_components, h, w))
+  # print("pca components:",pca.components_)
+  # print(pca.components_.shape)
+  # print("explained variance is: ",pca.explained_variance_)
+  # print("explained variance ration is:", pca.explained_variance_ratio_)
+  eigenfaces = pca.components_.reshape((i, h, w))
+  # print("shape of eigenfaces after reshaping",eigenfaces.shape)
 
-print "Projecting the input data on the eigenfaces orthonormal basis"
-t0 = time()
-X_train_pca = pca.transform(X_train)
-X_test_pca = pca.transform(X_test)
-print "done in %0.3fs" % (time() - t0)
+  # print("Projecting the input data on the eigenfaces orthonormal basis")
+  t0 = time()
+  X_train_pca = pca.transform(X_train)
+  X_test_pca = pca.transform(X_test)
+  print("done in %0.3fs" % (time() - t0))
+  # print("shape of x_train",X_train.shape)
+  # print("shape of X_train_pca:",X_train_pca.shape)
+  # print(X_train_pca)
+  # print("shape of x test pca:",X_test_pca.shape)
+  # print(X_test_pca)
+
+  ###############################################################################
+  # Train a SVM classification model
+
+  print("Fitting the classifier to the training set")
+  t0 = time()
+  param_grid = {
+          'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+            'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+            }
+  # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
+  clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+  print("shape of y_train is:",y_train.shape)
+  clf = clf.fit(X_train_pca, y_train)
+  print("done in %0.3fs" % (time() - t0))
+  print("Best estimator found by grid search:")
+  print(clf.best_estimator_)
 
 
-###############################################################################
-# Train a SVM classification model
+  ###############################################################################
+  # Quantitative evaluation of the model quality on the test set
 
-print "Fitting the classifier to the training set"
-t0 = time()
-param_grid = {
-         'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-          'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
-          }
-# for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
-clf = clf.fit(X_train_pca, y_train)
-print "done in %0.3fs" % (time() - t0)
-print "Best estimator found by grid search:"
-print clf.best_estimator_
+  print("Predicting the people names on the testing set")
+  t0 = time()
+  y_pred = clf.predict(X_test_pca)
+  print("done in %0.3fs" % (time() - t0))
 
-
-###############################################################################
-# Quantitative evaluation of the model quality on the test set
-
-print "Predicting the people names on the testing set"
-t0 = time()
-y_pred = clf.predict(X_test_pca)
-print "done in %0.3fs" % (time() - t0)
-
-print classification_report(y_test, y_pred, target_names=target_names)
-print confusion_matrix(y_test, y_pred, labels=range(n_classes))
+  print(classification_report(y_test, y_pred, target_names=target_names))
+  #print(confusion_matrix(y_test, y_pred, labels=list(range(n_classes))))
 
 
 ###############################################################################
