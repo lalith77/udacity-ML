e426d16e7a1261c88598d7e02ad490a9e8801579
Lalith Nimmagadda <lnimmagadda@DSRMINC.LOCAL>
2020-01-06 16:39:00 -0600

    changes to validate

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,8 +15,9 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -27,5 +28,19 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+clf = tree.DecisionTreeClassifier()
+print("Training data ...")
+print("The number of features are", len(features_train))
+print(features[0])
+
+clf.fit(features_train, labels_train)
+
+my_predictions = clf.predict(features_test)
+print("the predictions are:",my_predictions)
+print("test labels are:",labels_test)
+
+print(clf.score(features_test, labels_test))
 
 
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,8 +14,10 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn import tree
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
@@ -27,6 +29,17 @@ labels, features = targetFeatureSplit(data)
 
 
 
+
 ### it's all yours from here forward!  
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+clf = tree.DecisionTreeClassifier()
+print("Training data ...")
+print("The number of features are", len(features_train))
+print(features[0])
+
+clf.fit(features_train, labels_train)
+
+print(clf.score(features_test, labels_test))
 
 
